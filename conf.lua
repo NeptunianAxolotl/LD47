7751eacaaea978497546ce3c37a4bf8ca81a90d8
GoogleFrog <googl3frog@gmail.com>
2020-10-03 11:48:52 +1000

    Test 2.

--- conf.lua
+++ conf.lua
@@ -1,11 +1,10 @@
 function love.conf(t)
-    t.window.title = "LD47"
-    t.window.width = 1280
-    t.window.height = 768
-    --t.window.fullscreen = true -- Do not fullscreen since we lack an exit button.
-    t.window.resizable = false
-    --t.window.icon = "Images/icon.png"
+	t.window.title = "LD47"
+	t.window.width = 1280
+	t.window.height = 768
+	--t.window.fullscreen = true -- Do not fullscreen since we lack an exit button.
+	t.window.resizable = false
+	--t.window.icon = "Images/icon.png"
 
-    t.modules.joystick = false
+	t.modules.joystick = false
 end
-
