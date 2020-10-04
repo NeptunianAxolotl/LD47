323411c84a99cd91a1bdbf69cc1fc72aba564cfd
Istaera <istaera@gmail.com>
2020-10-04 23:20:17 +1100

    rocket for the bear to shoot (images are done)

---
+++ resources/rocket.lua
@@ -0,0 +1,17 @@
+return {
+	form = "iso_image",
+	xScale = 0.18,
+	yScale = 0.18,
+	firstDir = 0,
+	rotate = true,
+	files = {
+		"resources/images/rocket_e.png",
+		"resources/images/rocket_se.png",
+		"resources/images/rocket_s.png",
+		"resources/images/rocket_sw.png",
+		"resources/images/rocket_w.png",
+		"resources/images/rocket_nw.png",
+		"resources/images/rocket_n.png",
+		"resources/images/rocket_ne.png",
+	},
+}
