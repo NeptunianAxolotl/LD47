05fdfca52b9da813d2db2a4a9355441e8174a9ad
Istaera <istaera@gmail.com>
2020-10-04 22:58:27 +1100

    car

---
+++ resources/car.lua
@@ -0,0 +1,17 @@
+return {
+	form = "iso_image",
+	xScale = 0.18,
+	yScale = 0.18,
+	firstDir = 0,
+	rotate = true,
+	files = {
+		"resources/images/car_e.png",
+		"resources/images/car_se.png",
+		"resources/images/car_s.png",
+		"resources/images/car_sw.png",
+		"resources/images/car_w.png",
+		"resources/images/car_nw.png",
+		"resources/images/car_n.png",
+		"resources/images/car_ne.png",
+	},
+}
