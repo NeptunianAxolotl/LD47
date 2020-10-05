e898e6507452b76957e5eae1ef72f86a82f61d2c
Aquanim <threeoneteacup@gmail.com>
2020-10-06 00:37:45 +1100

    Set game window icon.

--- conf.lua
+++ conf.lua
@@ -1,10 +1,10 @@
 function love.conf(t)
-	t.window.title = "LD47"
+	t.window.title = "Crocodial"
 	t.window.width = 1280
 	t.window.height = 768
 	--t.window.fullscreen = true -- Do not fullscreen since we lack an exit button.
 	t.window.resizable = true
-	--t.window.icon = "Images/icon.png"
+	t.window.icon = "icon.png"
 
 	t.modules.joystick = false
     --t.window.fullscreen = true 
