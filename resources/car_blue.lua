f6b63e8db095c53f67a785234bf57e2b275032a7
Istaera <istaera@gmail.com>
2020-10-05 13:34:40 +1100

    added blue car

---
+++ resources/car_blue.lua
@@ -0,0 +1,17 @@
+return {
+	form = "iso_image",
+	xScale = 0.25,
+	yScale = 0.25,
+	firstDir = 0,
+	rotate = false,
+	files = {
+		"resources/images/car_blue_e.png",
+		"resources/images/car_blue_se.png",
+		"resources/images/car_blue_s.png",
+		"resources/images/car_blue_sw.png",
+		"resources/images/car_blue_w.png",
+		"resources/images/car_blue_nw.png",
+		"resources/images/car_blue_n.png",
+		"resources/images/car_blue_ne.png",
+	},
+}
