6422b1c36fce1ad979fe5e339959b00e5426cbc8
Istaera <istaera@gmail.com>
2020-10-05 19:36:14 +1100

    changed UI more

--- resources/shape_n.lua
+++ resources/shape_n.lua
@@ -6,8 +6,8 @@ for i = 3, 8 do
 		form = "image",
 		name = "shape_" .. i,
 		file = "resources/images/shape_" .. i .. ".png",
-		xScale = 0.7,
-		yScale = 0.7,
+		xScale = 0.9,
+		yScale = 0.9,
 		yOffset = 0.64
 	}
 end
