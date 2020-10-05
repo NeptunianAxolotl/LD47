9a60b7c6d991238adc144001dba37e3c1a80cc3b
Istaera <istaera@gmail.com>
2020-10-05 11:42:02 +1100

    made bunny images

---
+++ resources/bunny.lua
@@ -0,0 +1,17 @@
+return {
+	form = "iso_image",
+	xScale = 0.18,
+	yScale = 0.18,
+	firstDir = 0,
+	rotate = true,
+	files = {
+		"resources/images/bunny_e.png",
+		"resources/images/bunny_se.png",
+		"resources/images/bunny_s.png",
+		"resources/images/bunny_sw.png",
+		"resources/images/bunny_w.png",
+		"resources/images/bunny_nw.png",
+		"resources/images/bunny_n.png",
+		"resources/images/bunny_ne.png",
+	},
+}
