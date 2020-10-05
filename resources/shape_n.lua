1404dc4a0aac0f31bcb8e99c421aed4f26d71a75
Istaera <istaera@gmail.com>
2020-10-05 19:52:23 +1100

    changed UI, added spell colours

--- resources/shape_n.lua
+++ resources/shape_n.lua
@@ -6,8 +6,8 @@ for i = 3, 8 do
 		form = "image",
 		name = "shape_" .. i,
 		file = "resources/images/shape_" .. i .. ".png",
-		xScale = 0.9,
-		yScale = 0.9,
+		xScale = 0.8,
+		yScale = 0.8,
 		yOffset = 0.64
 	}
 end
